65e8778048a1b8c83c5c94e5a99311280f4a75a7
augustoalbano <augustoalbano@hotmail.com>
2020-03-27 13:17:44 -0300

    Projeto final

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,32 +1,27 @@
-const generateUniqueId = require('../utils/generateUniqueId');
-const connection = require('../database/connection');
-const crypto = require('crypto'); // importa a biblioteca crypto para gerarmos o id
+const generateUniqueId = require("../utils/generateUniqueId");
+const connection = require("../database/connection");
 
 module.exports = {
-    // SELECT: lista todas as ONG's cadastradas no banco de dados. Geralmente, o método de listar é chamado de index.
-    async index(request, response) {
-        const ongs = await connection('ongs').select('*');
-       
-        return response.json(ongs);
-    },
+  async index(req, res) {
+    const ongs = await connection("ongs").select("*");
 
-    // INSERT: cadastra/insere uma nova ONG no banco de dados
-    async create (request, response){
-        const { name, email, whatsapp, city, uf } = request.body;
+    return res.json(ongs);
+  },
 
-        // Gerar ID aleatório com 4 letras, utilizando a biblioteca crypto
-        const id = generateUniqueId();
-    
-        await connection('ongs').insert({
-            id,
-            name,
-            email,
-            whatsapp,
-            city,
-            uf,
-        })
-    
-    
-        return response.json({ id });
-    }
+  async store(req, res) {
+    const { name, email, whatsapp, city, uf } = req.body;
+
+    const id = generateUniqueId();
+
+    await connection("ongs").insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    });
+
+    return res.json({ id });
+  }
 };
